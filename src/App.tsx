7bb15ee03f77ce16020074bb07ad673c4cbdcbb3
Igor Souza <igorsouza.dev@gmail.com>
2020-03-16 16:17:55 -0300

    Adding api call

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import api from './services/api';
+interface IUser {
+  name: string;
+  email: string;
+}
 function App() {
+  const [users, setUsers] = useState<IUser[]>([]);
+  useEffect(()=>{
+    api.get<IUser[]>('/users').then(response => {
+      setUsers(response.data);
+    })
+  }, []);
   return (
-    <div>Hello world</div>
+    <div>
+      {users.map(user => (<div>{user.name}</div>))}
+    </div>
+    
   );
 }
 
